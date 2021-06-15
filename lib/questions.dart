2213d0b2edafe7793c34b4f32b48594987fb5896
mosfeqanik <mosfeqanik01@gmail.com>
2021-06-15 20:33:18 +0600

    constructor late add

---
+++ lib/questions.dart
@@ -0,0 +1,12 @@
+class Questions {
+
+  late String questionText;
+  late bool questionAnswer;
+
+  Questions ({required String fullQuestion,required bool answer}) {
+    questionText = fullQuestion;
+    questionAnswer = answer;
+  }
+
+
+}
