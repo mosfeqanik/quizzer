d0068ea0dbf463e426da6f2071a1dedaf2d661f0
mosfeqanik <mosfeqanik01@gmail.com>
2021-06-23 12:50:47 +0600

    project Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
-import 'questions.dart';
 import 'querstions_bank.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-
-void main() =>runApp(quiz());
+void main() => runApp(quiz());
 
 class quiz extends StatelessWidget {
   const quiz({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // int score=0;
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.cyanAccent,
-            title: Text("Quiz"),
-          ),
-          body: quizbody(),
-        )
-    );
+      appBar: AppBar(
+        backgroundColor: Colors.cyanAccent,
+        title: Text("Quiz      score="),
+      ),
+      body: quizbody(),
+    ));
   }
 }
-
-
 
 class quizbody extends StatefulWidget {
   const quizbody({Key? key}) : super(key: key);
@@ -32,15 +29,34 @@ class quizbody extends StatefulWidget {
 }
 
 class _quizbodyState extends State<quizbody> {
+  QuizCircuit Quizbrain = QuizCircuit();
 
-  QuizCircuit Quizbank= QuizCircuit();
+  // score=0;
+  List<Icon> scorekeeper = [];
 
-  List<Icon> scorekeeper=[
-    Icon(Icons.close_sharp,color: Colors.redAccent,),
-    Icon(Icons.done_sharp,color: Colors.lightGreen,)
-  ];
-
-  int questionNumber = 0;
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = Quizbrain.getCorrectAnswer();
+    setState(() {
+      if (userPickedAnswer == correctAnswer) {
+        scorekeeper.add(Icon(Icons.done_sharp, color: Colors.lightGreen));
+        // score=score+1;
+      } else {
+        scorekeeper.add(Icon(Icons.close_sharp, color: Colors.redAccent));
+      }
+      if (Quizbrain.isFinish() == true) {
+        Alert(
+          context: context,
+          title: "সমাপ্ত",
+          desc: "আপনার কুইজটি শেষ হল",
+        ).show();
+        Quizbrain.reset();
+        scorekeeper = [];
+        // score=0;
+      }
+        Quizbrain.nextQuestion();
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,58 +65,49 @@ class _quizbodyState extends State<quizbody> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(
-            flex: 5,
+            flex: 6,
             child: Padding(
               padding: EdgeInsets.all(2),
               child: Center(
-                  child: Text(Quizbank.getQuestionText(questionNumber),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25),
-                  )
-              ),
-            )
-        ),
+                  child: Text(
+                Quizbrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              )),
+            )),
         Expanded(
             child: Container(
-              margin: EdgeInsets.fromLTRB(20,0,20,0),
-              color: Colors.green,
-              child: TextButton(
-                child: Text('True',
-                  style: TextStyle(color: Colors.white,fontSize: 35),
-                ),
-                onPressed: () {
-                  bool correctAnswer =Quizbank.questions[questionNumber].questionAnswer ;
-                  if(correctAnswer==true){
-                    print("user got right answer");
-                  }else
-                    {
-                      print("user got wrong answer");
-                    }
-                  setState(() {
-                    questionNumber=questionNumber+1;
-                  });
-                },
-
-              ),
-            ) ),
-        SizedBox(height:6),
+          margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          color: Colors.green,
+          child: TextButton(
+            child: Text(
+              'সত্য',
+              style: TextStyle(color: Colors.white, fontSize: 35),
+            ),
+            onPressed: () {
+              checkAnswer(true);
+            },
+          ),
+        )),
+        SizedBox(height: 6),
         Expanded(
             child: Container(
-              margin: EdgeInsets.fromLTRB(20,0,20,0),
-              color: Colors.red,
-              child: TextButton(
-                child: Text('False',
-                  style: TextStyle(color: Colors.white,fontSize: 35),
-                ),
-                onPressed: () {
-                  questionNumber=questionNumber+1;
-                },
-              ),
-            ) ),
-        Row(children: scorekeeper)
+          margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          color: Colors.red,
+          child: TextButton(
+            child: Text(
+              'মিথ্যা',
+              style: TextStyle(color: Colors.white, fontSize: 35),
+            ),
+            onPressed: () {
+              checkAnswer(false);
+            },
+          ),
+        )),
+        Row(
+          children: scorekeeper,
+        )
       ],
     );
   }
 }
-
-
--- lib/querstions_bank.dart
+++ lib/querstions_bank.dart
@@ -1,16 +1,42 @@
 import 'questions.dart';
 
-class QuizCircuit{
-  List<Questions> _questions=[
-    Questions(fullQuestion:'d',answer:true),
-    Questions(fullQuestion:'e',answer:false),
-    Questions(fullQuestion:'f',answer:false),
-    Questions(fullQuestion:'g',answer:true),
+class QuizCircuit {
+  int _questionNumber = 0;
+  List<Questions> _questions = [
+    Questions(fullQuestion: '১.সোনার তরী কি কাজী নজরুল ইসলাম লিখেছেন ?', answer: false),
+    Questions(fullQuestion: '২. ১৯২২ সালে ১২ তারিখে কি ধুমকেতু কাজী নজরুল ইসলাম প্রকাশ করেছেন ?  ', answer: true),
+    Questions(fullQuestion: '৩.নজরুলের বন্দী অবস্থায় থাকায় রবীন্দ্রনাথ ঠাকুর কি বসন্ত গীতিনাট্যটি লিখেন?', answer: true),
+    Questions(fullQuestion: '৪.নজরুল কি ভাঙ্গার গান লিখেছেন?', answer: true),
+    Questions(fullQuestion: '৫.কাজী নজরুল ইসলাম ছদ্মনাম কি ভানুসিংহ ঠাকুর?', answer: false),
+    Questions(fullQuestion: '৬.চতুরঙ্গ কি কাজী নজরুল ইসলাম লিখেছেন? ', answer: false),
+    Questions(fullQuestion: '৭.গীতাঞ্জলি কি কাব্যগ্রন্থ?', answer: true),
+    Questions(fullQuestion: '৮.রবীন্দ্রনাথ ঠাকুর গীতাঞ্জলি কি ১৯১৩ সালে প্রকাশিত হয়?', answer: false),
+    Questions(fullQuestion: '৯.রবীন্দ্রনাথ ঠাকুরের লিখা প্রথম ছোটগল্প কি ছুটি?', answer: false),
+    Questions(fullQuestion: '১০.আত্মচরিত ঈশ্বরচন্দ্র বিদ্যা সাগরের আত্ন জীবনী?', answer: true),
+
+
   ];
 
-  String getQuestionText(int questionNumber){
-    return _questions[questionNumber].questionText;
+  String getQuestionText() {
+    return _questions[_questionNumber].questionText;
   }
 
-}
+  bool getCorrectAnswer() {
+    return _questions[_questionNumber].questionAnswer;
+  }
 
+  void nextQuestion() {
+    if (_questionNumber <= _questions.length - 1) _questionNumber++;
+  }
+
+  bool isFinish() {
+    if (_questionNumber == _questions.length-1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void reset(){
+    _questionNumber = 0;
+  }
+}
